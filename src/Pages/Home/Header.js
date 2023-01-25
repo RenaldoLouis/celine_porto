481481e433bfb59f4cdd9b5d34cdc74eae72bbdc
Renaldo Louis <renaldo.louis@commsult.id>
2023-01-25 15:42:53 +0700

    fix responsiveness for header image

--- src/Pages/Home/Header.js
+++ src/Pages/Home/Header.js
@@ -10,53 +10,51 @@ const Header = () => {
         <>
             <div className="homeBackground">
                 <div className="headerContaier">
-                    <div>
-                        <div class="row">
+                    <div class="row">
 
-                            <div className="col-1">
-                                <p className="textLeftUp">currently open for freelance projects</p>
-                                <img className="iconLeft" src={FlowerIcon} alt="flowerIcon" ></img>
-                                <p className="textLeftDown">currently open for freelance projects</p>
-                            </div>
+                        <div className="col-1">
+                            <p className="textLeftUp">currently open for freelance projects</p>
+                            <img className="iconLeft" src={FlowerIcon} alt="flowerIcon" ></img>
+                            <p className="textLeftDown">currently open for freelance projects</p>
+                        </div>
 
-                            <div class="col-7" >
-                                <div class="containerTwoLine">
-                                    <div class="boxTwoLine">
-                                        <img src={CelineTitle} style={{ width: '25rem' }} alt="CelineIcon" ></img>
-                                    </div>
-                                </div>
-
-                                <div>
-                                    A generalist designer that loves to explore interaction between UI, UX, and human.
-                                </div>
-                                <div style={{ marginBottom: '25px' }}>
-                                    Sometimes do motion & branding.
-                                </div>
-
-                                <div className="row" >
-                                    <div className="col headerText headerText-selected">
-                                        works
-                                    </div>
-                                    <div className="col" />
-                                </div>
-                                <hr />
-
-                                <div className="row" >
-                                    <div className="col" />
-                                    <div className="col headerText">
-                                        Contact
-                                        <hr />
-                                    </div>
+                        <div class="col" >
+                            <div class="containerTwoLine">
+                                <div class="boxTwoLine">
+                                    <img src={CelineTitle} style={{ width: '25rem' }} alt="CelineIcon" ></img>
                                 </div>
                             </div>
 
-                            <div class="col-4 imageHeaderContainer" >
-                                <img style={{ width: '100%' }} src={ComingSoonHeader} alt="ComingSoon" ></img>
+                            <div>
+                                A generalist designer that loves to explore interaction between UI, UX, and human.
+                            </div>
+                            <div style={{ marginBottom: '25px' }}>
+                                Sometimes do motion & branding.
+                            </div>
+
+                            <div className="row" >
+                                <div className="col headerText headerText-selected">
+                                    works
+                                </div>
+                                <div className="col" />
+                            </div>
+                            <hr />
+
+                            <div className="row" >
+                                <div className="col" />
+                                <div className="col headerText">
+                                    Contact
+                                    <hr />
+                                </div>
                             </div>
                         </div>
-                        <div className="circle" >
+
+                        <div class="col imageHeaderContainer" >
+                            <img style={{ width: '100%' }} src={ComingSoonHeader} alt="ComingSoon" ></img>
+                        </div>
+                        {/* <div className="circle" >
                             <img src={FlowerBlueIconSmall} alt="flowerIcon" ></img>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
