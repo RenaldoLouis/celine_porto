e68867dde805d091187e7c08a2ac775473c1e5d9
Renaldo Louis <renaldo.louis@commsult.id>
2023-02-12 18:57:23 +0100

    fix moving text 2 line on small screen and add basic box on what i do

--- src/Pages/Home/WhatIDoContent.js
+++ src/Pages/Home/WhatIDoContent.js
@@ -3,8 +3,24 @@ import React from "react";
 export const WhatIDoContent = () => {
 
     return (
-        <div className="rowCenter" style={{ color: "white", fontSize: 64, fontWeight: 300 }}>
-            What I Do
+        <div>
+            <div className="rowCenter" style={{ color: "white", fontSize: 64, fontWeight: 300 }}>
+                What I Do
+            </div>
+            <div className="container px-4">
+                <div className="row" style={{ height: 500 }}>
+                    <div className="col" >
+                        <div style={{ color: "white", backgroundColor: "red", height: "100%", width: "100%" }}>
+                            asd
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div style={{ color: "white", backgroundColor: "red", height: "100%", width: "100%" }}>
+                            asd
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
